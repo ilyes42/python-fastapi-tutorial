dfce3fb2e437304680e95524be6c57f979477171
ilyes42 <ilyeshammemi42@gmail.com>
2021-11-01 20:27:49 +0100

    add some routes

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-  return {"message": "Hello FastAPI !"}
+@app.get("/blogs")
+def blogs():
+  return {"data": "blogs list"}
+
+
+@app.get("/blogs/unpublished")
+def unpublished():
+  # fetch all unpublished blogs
+  return {"data": "unpublished blogs"}
+
+
+@app.get("/blogs/{id}")
+def blog(id: int):
+  # fetch blog with id = id
+  return {"data": id}
+
+
+@app.get("/blogs/{id}/comments")
+def comments(id: int):
+  # fetch comments of blog with id = id
+  return {"data": f"comments of blog with id {id}"}
